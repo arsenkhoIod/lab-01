4bf946b6dbdaba1460e064448570ef62870f774e
Arseny <arsenykholod@yandex.ru>
2020-10-04 21:04:40 +0300

    version 3 (with MORE simple tests)

--- tests/test.cpp
+++ tests/test.cpp
@@ -12,8 +12,8 @@ TEST(test_avg, test_avg_cast){
 }
 
 TEST(test_avg, test_avg_cast_1){
-  std::any a{std::string("4.2")};
-  EXPECT_EQ(cast_avg(a),"| 4.2");
+  std::any a{std::string("4.25")};
+  EXPECT_EQ(cast_avg(a),"| 4.25");
 }
 
 TEST(test_debt, test_debt_cast){
@@ -21,9 +21,20 @@ TEST(test_debt, test_debt_cast){
   EXPECT_EQ(cast_debt(a),"| null");
 }
 
+TEST(test_debt, test_avg_cast_1){
+  std::any a{std::string("C++")};
+  EXPECT_EQ(cast_debt(a),"| C++");
+}
 
+TEST(test_group, test_group_cast){
+  std::any a(size_t(31));
+  EXPECT_EQ(cast_group(a),"| 31 ");
+}
 
-
+TEST(test_group, test_group_cast_1){
+  std::any a{std::string("IU8-31")};
+  EXPECT_EQ(cast_group(a),"| IU8-31");
+}
 
 /*TEST(test_avg, test_avg_cast){
   std::any a(size_t(4));
